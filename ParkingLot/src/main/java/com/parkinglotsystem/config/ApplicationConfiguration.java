88cf2ad4613b46a5f4b1fce22f2a4d6f32c9b2bf
Your Name <nnchavadi2@gmail.com>
2020-06-17 19:35:38 +0530

    [niranjanchavadi]ADDED to encode the password for given password

---
+++ ParkingLot/src/main/java/com/parkinglotsystem/config/ApplicationConfiguration.java
@@ -0,0 +1,36 @@
+package com.parkinglotsystem.config;
+
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class ApplicationConfiguration {
+	
+	private static MessageSourceAccessor messageSourceAccessor;
+	
+	 @PostConstruct
+   private static void initMessageSourceAccessor() {
+       ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+       messageSource.setBasenames("classpath:messages");
+       messageSourceAccessor = new MessageSourceAccessor(messageSource, Locale.getDefault());
+   }
+
+   public static MessageSourceAccessor getMessageAccessor() {
+       return messageSourceAccessor;
+   }
+	
+	@Bean
+	public BCryptPasswordEncoder getPasswordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+
+
+}
