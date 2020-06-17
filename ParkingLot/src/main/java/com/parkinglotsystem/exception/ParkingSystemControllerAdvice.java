a483004f9c60416b29fd507e2ae17b2d86b8fc9a
Your Name <nnchavadi2@gmail.com>
2020-06-17 23:09:10 +0530

    [niranjanchavadi]ADDED handled exception thrown by the parkinglot controller for particular type

---
+++ ParkingLot/src/main/java/com/parkinglotsystem/exception/ParkingSystemControllerAdvice.java
@@ -0,0 +1,29 @@
+package com.parkinglotsystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.parkinglotsystem.response.ParkingSystemResponse;
+
+@ControllerAdvice
+public class ParkingSystemControllerAdvice extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ParkingSystemExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+		ParkingSystemExceptionResponse exceptionResponse = new ParkingSystemExceptionResponse(ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(ParkingSystemException.class)
+	public final ResponseEntity<ParkingSystemExceptionResponse> handleBadRequestException(ParkingSystemResponse ex,
+			WebRequest request) {
+		ParkingSystemExceptionResponse exceptionResponse = new ParkingSystemExceptionResponse(ex.getMsg(),
+				request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+}
